89933e5b276005c6df51d2e1a62e02c21b817c7e
Guigxs <guigxs19@gmail.com>
2021-11-22 14:59:17 +0100

    add couchdb

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-const express =require("express")
+const NodeCouchDb = require('node-couchdb');
+const express = require("express")
+
+// const couch = new NodeCouchDb();
+// const MemcacheNode = require('node-couchdb-plugin-memcached');
+// const couchWithMemcache = new NodeCouchDb({
+//     cache: new MemcacheNode
+// });
+
+// node-couchdb instance talking to external service
+const couchExternal = new NodeCouchDb({
+    host: 'couchdb.external.service',
+    protocol: 'https',
+    port: 5984
+});
+ 
+// not admin party
+const couchAuth = new NodeCouchDb({
+    auth: {
+        user: 'admin',
+        pass: 'admin'
+    }
+});
+
 const app = express()
 
 app.get('/',function(req,res){
     res.send('Hello World')
+    
 })
 
 app.listen(8090)
